0e4afbe01675422677b1e4e6099ca7b8d5ffaf39
jadeboy33 <caleb.otey@faulkner.edu>
2022-10-28 09:09:52 -0500

    Created unit conversion application and added support to convert inches to centimeters and to convert centimeters to inches.

--- Git Assignment/Program.cs
+++ Git Assignment/Program.cs
@@ -10,6 +10,32 @@ namespace Git_Assignment
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter the unit you would like to convert. Type 'in' to convert inches to " +
+                "centimeters, and 'cm'  to convert centimeters to inches.");
+
+            String unitType = Console.ReadLine();
+            while (unitType != "inches" && unitType != "centimeters")
+            {
+                unitType = Console.ReadLine();
+            }
+
+            Console.WriteLine("How many " + unitType + "s would you like to convert?");
+            String unitAmtStr = Console.ReadLine();
+            int unitAmtInt = Int32.Parse(unitAmtStr);
+
+            if (unitType == "in") //Converts inches to centimeters
+            {
+                double newUnit = 2.54 * unitAmtInt;
+                Console.WriteLine("You now have " + newUnit + "centimeters.");
+            }
+
+            else if (unitType == "cm") //Converts cm to inches
+            {
+                double newUnit = unitAmtInt / 2.54;
+                Console.WriteLine("You now have " + newUnit + "inches");
+
+            }
+
         }
     }
 }
