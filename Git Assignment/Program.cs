5bed5ce84d0f215d502343d4d7645bac4b27b560
jadeboy33 <caleb.otey@faulkner.edu>
2022-11-01 12:53:29 -0500

    Added conversions from meters to feet and from feet to meters.

--- Git Assignment/Program.cs
+++ Git Assignment/Program.cs
@@ -11,29 +11,37 @@ namespace Git_Assignment
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter the unit you would like to convert. Type 'in' to convert inches to " +
-                "centimeters, and 'cm'  to convert centimeters to inches.");
+                "centimeters, 'cm'  to convert centimeters to inches, 'mt' to convert meters to feet, and 'ft' to convert feet " +
+                "to meters.");
 
             String unitType = Console.ReadLine();
-            while (unitType != "inches" && unitType != "centimeters")
-            {
-                unitType = Console.ReadLine();
-            }
 
             Console.WriteLine("How many " + unitType + "s would you like to convert?");
             String unitAmtStr = Console.ReadLine();
             int unitAmtInt = Int32.Parse(unitAmtStr);
-
+          
             if (unitType == "in") //Converts inches to centimeters
             {
                 double newUnit = 2.54 * unitAmtInt;
-                Console.WriteLine("You now have " + newUnit + "centimeters.");
+                Console.WriteLine("You now have " + newUnit + " centimeters.");
             }
 
             else if (unitType == "cm") //Converts cm to inches
             {
                 double newUnit = unitAmtInt / 2.54;
-                Console.WriteLine("You now have " + newUnit + "inches");
+                Console.WriteLine("You now have " + newUnit + " inches");
+            }
 
+            else if (unitType == "mt") //Converts meters to feet
+            {
+                double newUnit = 3.28 * unitAmtInt;
+                Console.WriteLine("You now have " + newUnit + " feet");
+            }
+
+            else if (unitType == "ft") //Converts meters to feet
+            {
+                double newUnit = 3.28 * unitAmtInt;
+                Console.WriteLine("You now have " + newUnit + " feet");
             }
 
         }
